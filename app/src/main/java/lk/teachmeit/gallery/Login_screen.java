1b5edf41cd71a91d27bcc1150c0e93d932b5ec2f
Tharuka Lakshan <tharukawapnet@gmail.com>
2018-08-04 16:48:56 +0530

    Fresh project commitment

---
+++ app/src/main/java/lk/teachmeit/gallery/Login_screen.java
@@ -0,0 +1,37 @@
+package lk.teachmeit.gallery;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class Login_screen extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+    TextView credit;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+            credit.setVisibility(View.GONE);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+        rellay1 = findViewById(R.id.rellay1);
+        rellay2 = findViewById(R.id.rellay2);
+        credit = findViewById(R.id.credits);
+
+        handler.postDelayed(runnable, 5000);
+
+    }
+}
---
+++ app/src/main/java/lk/teachmeit/gallery/SignUp_Screen.java
@@ -0,0 +1,13 @@
+package lk.teachmeit.gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up__screen);
+    }
+}
