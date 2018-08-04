f07d91b2c957ec347df6fd7ea0451361ff750eb9
Tharuka Lakshan <tharukawapnet@gmail.com>
2018-08-04 17:21:35 +0530

    Updated Sign up xml

--- app/src/main/java/lk/teachmeit/gallery/Login_screen.java
+++ app/src/main/java/lk/teachmeit/gallery/Login_screen.java
@@ -4,10 +4,12 @@
 
 package lk.teachmeit.gallery;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -15,6 +17,7 @@ public class Login_screen extends AppCompatActivity {
 
     RelativeLayout rellay1, rellay2;
     TextView credit;
+    Button login;
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
@@ -34,8 +37,17 @@ public class Login_screen extends AppCompatActivity {
         rellay1 = findViewById(R.id.rellay1);
         rellay2 = findViewById(R.id.rellay2);
         credit = findViewById(R.id.credits);
+        login = findViewById(R.id.signup_log_btn);
 
         handler.postDelayed(runnable, 5000);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login_screen.this, SignUp_Screen.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
