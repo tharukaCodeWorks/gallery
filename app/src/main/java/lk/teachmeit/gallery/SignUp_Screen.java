c41507566895e2063d495bf27117ed6df80ea6d5
Tharuka Lakshan <tharukawapnet@gmail.com>
2018-08-04 17:33:14 +0530

    Updated Sign up xml

--- app/src/main/java/lk/teachmeit/gallery/SignUp_Screen.java
+++ app/src/main/java/lk/teachmeit/gallery/SignUp_Screen.java
@@ -2,12 +2,25 @@ package lk.teachmeit.gallery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp_Screen extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up__screen);
+
+        login = findViewById(R.id.loging_sig_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
